99fb922eac4ba215c0f0ba315a9a18849d829267
Jeb Boone <boo21006@byui.edu>
2025-03-01 13:47:53 -0700

    got the number guesser working

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random RandomGenerator = new Random();
+        int number_to_guess = RandomGenerator.Next(1, 101);
+
+        int user_guess = -1;
+        while (user_guess != number_to_guess)
+        {
+            Console.Write("what is your guess?: \n");
+            user_guess = int.Parse(Console.ReadLine());
+
+            if(user_guess > number_to_guess)
+            {
+                Console.Write("your guess is too high: \n");
+            }
+            else if (user_guess < number_to_guess)
+            {
+                Console.Write("your guess is too low: \n");
+            }
+            else
+            {
+                Console.Write("good job you got it!");
+            }
+        }
     }
 }
