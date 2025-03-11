29f9d3431d194308ffd372792bec76642ea196d6
Jeb Boone <boo21006@byui.edu>
2025-03-10 20:51:40 -0600

    prep is done

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionString = $"{_top}/{_bottom}";
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimalValue = _top / _bottom;
+        return decimalValue;
+    }
+}
