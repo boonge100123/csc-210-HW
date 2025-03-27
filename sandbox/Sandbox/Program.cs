fc197fc0be4d66307f99bb4f4b0f9d196c16f789
Jeb Boone <boo21006@byui.edu>
2025-03-27 09:27:14 -0600

    class 3/27

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,30 +1,44 @@
 using System;
 class Program
 {
-static void runspiner()
-{
-    int counter = 0;
-    while (counter < 10)
-    {
-        Console.Write(".");
-        Thread.Sleep(1000);
-        Console.Write(".");
-        Thread.Sleep(1000);
-        Console.Write(".");
-        Thread.Sleep(1000);
-        Console.Write("\b\b\b");
-        Console.Write("   ");
-        Console.Write("\b\b\b");
-        Thread.Sleep(1000);
+// static void runspiner()
+// {
+//     int counter = 0;
+//     while (counter < 10)
+//     {
+//         Console.Write(".");
+//         Thread.Sleep(1000);
+//         Console.Write(".");
+//         Thread.Sleep(1000);
+//         Console.Write(".");
+//         Thread.Sleep(1000);
+//         Console.Write("\b\b\b");
+//         Console.Write("   ");
+//         Console.Write("\b\b\b");
+//         Thread.Sleep(1000);
 
-        counter++;
-    }
-}
+//         counter++;
+//     }
+// }
 
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        runspiner();
+        // Console.WriteLine("Hello World!");
+        // runspiner();
+
+        List<int> numbers = new List<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        List<int> evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+        List<int> oddNumbers = numbers.Where(n => n % 2 != 0).ToList();
+
+        foreach (int x in evenNumbers)
+        {
+            Console.WriteLine(x);
+        }
+
+        foreach (int x in oddNumbers)
+        {
+            Console.WriteLine(x);
+        }
     }
 }
